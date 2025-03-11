d4b101ceb0de32cf26f209941ea5930c5b0a9c0a
Nate Schaffner <thx1766@gmail.com>
2025-03-11 08:53:38 -0400

    Initial Commit

---
+++ podbubble/podbubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  podbubbleApp.swift
+//  podbubble
+//
+//  Created by Nate Schaffner on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct podbubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
